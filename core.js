181d73dd59cdb915556d26820c2c45908904d833
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-08 17:34:03 +0200

    Updated structure & effects

--- core.js
+++ core.js
@@ -1,1 +1,22 @@
+const scrollEffect = (event) => {
+    console.log(event);
+}
 
+const $main = document.querySelector('header');
+const setScrollProgress = (progress = getScrollPercent()) => {    
+    if (progress < 20) $main.classList.remove('header--scrolled');
+    else $main.classList.add('header--scrolled')
+}
+
+function getScrollPercent() {
+    var h = document.documentElement, 
+        b = document.body,
+        st = 'scrollTop',
+        sh = 'scrollHeight';
+    return Math.round((h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100);
+}
+
+setScrollProgress();
+document.addEventListener('scroll', () => {
+    setScrollProgress();
+})
