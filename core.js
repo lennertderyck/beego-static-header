66523e5aed2ceffcaf2e62fba1dcc8f6853a9e4e
lennertderyck <32176355+gdm-1718-lenndery@users.noreply.github.com>
2020-10-09 10:59:39 +0200

    Update

--- core.js
+++ core.js
@@ -1,22 +1,24 @@
 const scrollEffect = (event) => {
-    console.log(event);
-}
+  console.log(event);
+};
 
-const $main = document.querySelector('header');
-const setScrollProgress = (progress = getScrollPercent()) => {    
-    if (progress < 20) $main.classList.remove('header--scrolled');
-    else $main.classList.add('header--scrolled')
-}
+const $main = document.querySelector("header");
+const setScrollProgress = (progress = getScrollPercent()) => {
+  if (progress < 20) $main.classList.remove("header--scrolled");
+  else $main.classList.add("header--scrolled");
+};
 
 function getScrollPercent() {
-    var h = document.documentElement, 
-        b = document.body,
-        st = 'scrollTop',
-        sh = 'scrollHeight';
-    return Math.round((h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 100);
+  const h = document.documentElement,
+    b = document.body,
+    st = "scrollTop",
+    sh = "scrollHeight";
+  return Math.round(
+    ((h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight)) * 100
+  );
 }
 
 setScrollProgress();
-document.addEventListener('scroll', () => {
-    setScrollProgress();
-})
+document.addEventListener("scroll", () => {
+  setScrollProgress();
+});
